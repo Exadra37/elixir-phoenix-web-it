40e624bd939359a2624130ac4ea58ff36e84ba58
Exadra37 <exadra37@gmail.com>
2020-06-09 22:47:23 +0100

    Big Bang :)
    Signed-off-by: Exadra37 <exadra37@gmail.com>

---
+++ README.md
@@ -0,0 +1,23 @@
+# PHOENIX WEB COMPONENTS
+
+A collection of web components for Phoenix projects.
+
+The web components will be normal Phoenix Html templates or full featured Live View ones.
+
+## INSTALLATION
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `phoenix_web_components` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:phoenix_web_components, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/phoenix_web_components](https://hexdocs.pm/phoenix_web_components).
+
---
+++ mix.exs
@@ -0,0 +1,69 @@
+defmodule PhoenixWebComponents.MixProject do
+  use Mix.Project
+
+  @package_name "PHOENIX WEB COMPONENTS"
+
+  @version "0.1.0"
+
+  @source_url "https://gitlab.com/exadra37-elixir/packages/phoenix-web-components"
+
+  @homepage_url "https://gitlab.com/exadra37-elixir/packages/phoenix-web-components/blob/master/README.md"
+
+  @description """
+  A package with web components for Phoenix projects, with Live View and without it.
+  """
+
+  def project() do
+    [
+      app: :phoenix_web_components,
+      version: @version,
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      build_embedded: Mix.env == :prod,
+      deps: deps(),
+
+      # Hex
+      package: package(),
+      description: @description,
+      source_url: @source_url,
+      homepage_url: @homepage_url,
+
+      # Docs
+      name: @package_name,
+      docs: [
+        source_ref: "v#{@version}",
+        source_url: @source_url,
+        main: "readme",
+        extras: ["README.md"]
+      ],
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+    ]
+  end
+
+  defp package() do
+    [
+      name: @package_name,
+
+      maintainers: ["Exadra37"],
+
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["MIT"],
+      links: %{"Gitlab" => @source_url}
+    ]
+  end
+end
